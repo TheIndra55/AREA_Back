af5e325079683f867b085421a620968ebace7b91
Xwilarg <xwilarg@yahoo.fr>
2019-01-14 11:30:20 +0100

    Add empty Nancy server

---
+++ AREA_Back/Program.cs
@@ -0,0 +1,33 @@
+﻿using Nancy.Hosting.Self;
+using System;
+using System.Threading;
+
+namespace AREA_Back
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AutoResetEvent autoEvent = new AutoResetEvent(false);
+            LaunchServer(autoEvent);
+            autoEvent.WaitOne();
+        }
+
+        private static void LaunchServer(AutoResetEvent autoEvent)
+        {
+            HostConfiguration config = new HostConfiguration()
+            {
+                UrlReservations = new UrlReservations() { CreateAutomatically = true }
+            };
+            NancyHost host = new NancyHost(config, new Uri("http://localhost:8080/"));
+            host.Start();
+            Console.WriteLine("Host Started... Do ^C to exit.");
+            Console.CancelKeyPress += (sender, e) =>
+            {
+                Console.WriteLine("^C received, exitting...");
+                host.Dispose();
+                autoEvent.Set();
+            };
+        }
+    }
+}
