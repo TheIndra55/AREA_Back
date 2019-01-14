e7c3fe3d3487309f4d5c5d98b0b141119f7b266c
Xwilarg <xwilarg@yahoo.fr>
2019-01-14 11:54:32 +0100

    Add basic about.json

---
+++ AREA_Back/Endpoint/About.cs
@@ -0,0 +1,33 @@
+﻿using Nancy;
+using System;
+
+namespace AREA_Back.Endpoint
+{
+    public class About : NancyModule
+    {
+        public About()
+        {
+            Get("/", x =>
+            {
+                return (Response.AsRedirect("/about.json"));
+            });
+
+            Get("/about.json", x =>
+            {
+                return (Response.AsJson(new Response.About()
+                {
+                    Client = new Response.PClient()
+                    {
+                        Host = Request.UserHostAddress
+                    },
+                    Server = new Response.PServer()
+                    {
+                        Current_time = ((int)((DateTime.Now - new DateTime(1970, 1, 1)).TotalSeconds)),
+                        Services = new Response.PService[]
+                        { }
+                    }
+                }));
+            });
+        }
+    }
+}
---
+++ AREA_Back/Response/About.cs
@@ -0,0 +1,49 @@
+﻿using Newtonsoft.Json;
+
+namespace AREA_Back.Response
+{
+    public class PInterraction
+    {
+        [JsonProperty]
+        public string Name;
+
+        [JsonProperty]
+        public string Description;
+    }
+
+    public class PService
+    {
+        [JsonProperty]
+        public string Name;
+
+        [JsonProperty]
+        public PInterraction[] Actions;
+
+        [JsonProperty]
+        public PInterraction[] Reactions;
+    }
+
+    public class PClient
+    {
+        [JsonProperty]
+        public string Host;
+    }
+
+    public class PServer
+    {
+        [JsonProperty]
+        public int Current_time;
+
+        [JsonProperty]
+        public PService[] Services;
+    }
+
+    public class About
+    {
+        [JsonProperty]
+        public PClient Client;
+
+        [JsonProperty]
+        public PServer Server;
+    }
+}
