e3f6c04fcfc28916f50c2a844008e342503ae93f
Xwilarg <xwilarg@yahoo.fr>
2019-02-20 15:37:27 +0100

    Change booru duplicate check with id

--- AREA_Back/Action/ABooru.cs
+++ AREA_Back/Action/ABooru.cs
@@ -9,23 +9,20 @@ namespace AREA_Back.Action
         public ABooru(BooruSharp.Booru.Booru booru) : base(25f)
         {
             this.booru = booru;
-            lastUrl = GetLastLink().GetAwaiter().GetResult();
+            id = booru.GetImage(0).GetAwaiter().GetResult().id;
         }
 
         public override void InternalUpdate(Action<string, string> action)
         {
-            string url = GetLastLink().GetAwaiter().GetResult();
-            if (lastUrl != url)
+            int lastId = booru.GetImage(0).GetAwaiter().GetResult().id;
+            if (lastId != id)
             {
-                action("A new image was uploaded on " + booru.ToString().Split('.').Last(), url);
-                lastUrl = url;
+                action("A new image was uploaded on " + booru.ToString().Split('.').Last(), booru.GetImage(0).GetAwaiter().GetResult().fileUrl.AbsoluteUri);
+                id = lastId;
             }
         }
 
-        private async Task<string> GetLastLink()
-            => (await booru.GetImage(0)).fileUrl.AbsoluteUri;
-
-        BooruSharp.Booru.Booru booru;
-        string lastUrl;
+        private BooruSharp.Booru.Booru booru;
+        private int id;
     }
 }
